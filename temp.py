87b612e799c5056510af0cd25b2cabf0b67325ca
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-06-07 17:15:34 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,9 @@
+num1=float(input('enter the first no '))
+num2=float(input('enter the second no '))
+num3=float(input('enter the third no'))
+sum=num1+num2+num3
+print('addition of the result is:'+str(sum))
+
+
+
+

835194fbdd0cb43d6881648dd7233a7f86756eab
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-06-08 17:21:25 +0530

    Add files via upload

---
+++ swapno.py
@@ -0,0 +1,9 @@
+num1=int(input('enter the first no to swap'))
+num2=int(input('enter the first no to swap'))
+temp=num1
+num1=num2
+num2=temp
+print('after swapping the first no is:'+str(num1))
+print('after swapping the first no is:'+str(num2))
+
+

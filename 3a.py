e80d05b2120c2a25073808911cf43411c382d571
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-07-08 12:44:33 +0530

    Add files via upload

---
+++ 3a.py
@@ -0,0 +1,15 @@
+sentence = input("enter a sentences")
+
+wordList = sentence.split(" ")
+print("this sentence has",len(wordList),"words")
+
+digcnt = upcnt = locnt = 0
+
+for ch in sentence:
+    if '0'<=ch<='9':
+        digcnt += 1
+    elif'A'<=ch<='Z':
+        upcnt += 1
+    elif 'a'<=ch<='z':
+            locnt += 1
+print("this sentence has",digcnt,"digits",upcnt,"uppercase",locnt,"lowercaseletter")

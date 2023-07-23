86e36877dec63e580f5d1f0b243ea7260d2d52ac
lekha poojary <117467060+poojary19@users.noreply.github.com>
2023-07-23 19:51:23 +0530

    Add files via upload

--- 4a.py
+++ 4a.py
@@ -9,44 +9,45 @@ def merge_sort(lst):
         merge_sort(right_half)
         i=j=k=0
         while i<len(left_half) and j<len(right_half):
-            if left_half[i]<right_half[j]:
-              lst[k]=left_half[i]
-              i+=1
-            else:
-              lst[k]=right_half[j]
-              j+=1
-            k+=1 
-        while i<len(left_half):
+         if left_half[i]<right_half[j]:
             lst[k]=left_half[i]
             i+=1
-            k+=1
-        while j<len(right_half):
+         else:
             lst[k]=right_half[j]
             j+=1
-            k+=1
+        k+=1
+        while i<len(left_half):
+          lst[k]=left_half[i]
+          i+=1
+          k+=1
+        while j<len(right_half):
+          lst[k]=right_half[j]
+          i+=1
+          k+=1
     return lst
-
 def insertion_sort(arr):
-    for i in range(1,len(arr)):
-        key=arr[i]
-        j=i-1
-        while j>=0 and key<arr[j]:
-            arr[j+1]=arr[j]
-            j-=1
-        arr[j+1]=key
+ for i in range(1,len(arr)):
+    key=arr[i]
+    j=i-1
+ while j>=0 and key<arr[j]:
+    arr[j+1]=arr[j]
+    j-=1
+    arr[j+1]=key
 my_list=[]
 for i in range(10):
-     my_list.append(random.randint(0,999))
+  my_list.append(random.randint(0,999))
 print("\n unsorted list")
 print(my_list)
 print("sorting string insertion sort")
 insertion_sort(my_list)
-print(my_list)
 my_list=[]
 for i in range(10):
-    my_list.append(random.randint(0,999))
+  my_list.append(random.randint(0,999))
 print("\n sorted list")
 print(my_list)
-print("sorting using merge sort")
+print("sorting string merge sort")
 merge_sort(my_list)
 print(my_list)
+                        
+        
+        
